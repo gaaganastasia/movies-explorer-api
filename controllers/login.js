875c8d9d4251ca1482948c2b71f925161b57614e
Anastasia <67376895+gaaganastasia@users.noreply.github.com>
2022-04-11 22:38:22 +0700

    Update login.js

--- controllers/login.js
+++ controllers/login.js
@@ -9,9 +9,7 @@ const login = (req, res, next) => {
     .then((user) => {
       const token = jwt.sign(
         { _id: user._id },
-        process.env.NODE_ENV === 'production'
-          ? process.env.JWT_SECRET
-          : 'dev-secret',
+        'dev-secret',
         { expiresIn: '7d' },
       );
       res.send({ token });
